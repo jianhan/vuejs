870610a3a3630db6bfa798d3d790fac42975584d
Jian Han <jianhan@mail.com>
2018-01-17 21:46:56 +1000

    setup auth0

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,61 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Auth0 - Vue</a>
+
+          <router-link :to="'/'"
+            class="btn btn-primary btn-margin">
+              Home
+          </router-link>
+
+          <button
+            class="btn btn-primary btn-margin"
+            v-if="!authenticated"
+            @click="login()">
+              Log In
+          </button>
+
+          <button
+            class="btn btn-primary btn-margin"
+            v-if="authenticated"
+            @click="logout()">
+              Log Out
+          </button>
+
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view
+        :auth="auth"
+        :authenticated="authenticated">
+      </router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import AuthService from '@/auth/service'
+const auth = new AuthService()
+const { login, logout, authenticated, authNotifier } = auth
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+    return {
+      auth,
+      authenticated
+    }
+  },
+  methods: {
+    login,
+    logout
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/callback.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="spinner">
+  </div>
+</template>
+
+<script>
+import AuthService from '@/auth/service'
+const auth = new AuthService()
+const { handleAuthentication } = auth
+
+export default {
+  name: 'callback',
+  data () {
+    handleAuthentication()
+    return {}
+  }
+}
+</script>
