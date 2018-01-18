22ee5a90d547bac014767f5bd1654c944e09696a
Jian Han <jianhan@mail.com>
2018-01-18 22:00:09 +1000

    added vue2 filters

--- src/main.js
+++ src/main.js
@@ -8,11 +8,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import moment from 'moment'
 import axios from '@/http/axios'
+import _ from 'lodash'
+import Vue2Filters from 'vue2-filters'
 
+Vue.use(Vue2Filters)
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Object.definePrototype(Vue.prototype, '$moment', { value: moment })
 Object.definePrototype(Vue.prototype, '$axios', { value: axios })
+Object.definePrototype(Vue.prototype, '$_', { value: _ })
 
 /* eslint-disable no-new */
 new Vue({
